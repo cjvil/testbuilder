43f940be762bca9027bbed3a47106bcfe5725a55
Christie Villanueva <christiejvillanueva@gmail.com>
2017-07-25 09:45:19 -0700

    Implement Visa and MasterCard ID

--- detectNetwork.js
+++ detectNetwork.js
@@ -11,6 +11,8 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
+  // Visa always has a prefix of 4 and a length of 13, 16, or 19.
+  // MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 
@@ -18,7 +20,11 @@ var detectNetwork = function(cardNumber) {
   	return 'Diner\'s Club';
   } else if (cardNumber.length === 15 && (cardNumber.substring(0, 2) === '34' || cardNumber.substring(0, 2) === '37')) {
   	return 'American Express';
+  } else if (cardNumber.charAt(0) === '4' && (cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19)) {
+  	return 'Visa';
+  } else if ( (parseInt(cardNumber.substring(0, 2)) <= 55 && parseInt(cardNumber.substring(0, 2)) >= 51) && (cardNumber.length === 16) ) {
+  	return 'MasterCard';
   }
+
+
 };
-
-
