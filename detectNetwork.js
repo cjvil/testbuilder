e9ddd33ac0c4034b76a6bf2950c462152625afbb
Christie Villanueva <christiejvillanueva@gmail.com>
2017-07-25 19:03:20 -0700

    Refactor detectNetwork using prefixInRange function
    Added prefixInRange function to improve readability.

--- detectNetwork.js
+++ detectNetwork.js
@@ -15,31 +15,32 @@ var detectNetwork = function(cardNumber) {
   // MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
   // Discover always has a prefix of 6011, 644-649, or 65, and a length of 16 or 19.
 	// Maestro always has a prefix of 5018, 5020, 5038, or 6304, and a length of 12-19.
+	// China UnionPay always has a prefix of 622126-622925, 624-626, or 6282-6288 and a length of 16-19.
+	// Switch always has a prefix of 4903, 4905, 4911, 4936, 564182, 633110, 6333, or 6759 and a length of 16, 18, or 19.
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 
+  var prefixInRange = function(min, max) {
+  	return (parseInt(cardNumber.substring(0, min.toString().length)) >= min) && (parseInt(cardNumber.substring(0, min.toString().length)) <= max);
+  }
+
   var isDiscover = function() {
-  	if (cardNumber.length !== 16 && cardNumber.length !== 19) {
+  	if ([16, 19].includes(cardNumber.length)) {
   		return false;
   	}
-  	if (cardNumber.substring(0, 4) === '6011' || cardNumber.substring(0, 2) === '65') {
-  		return true;
-  	}
-  	if (parseInt(cardNumber.substring(0, 3)) >= 644 && parseInt(cardNumber.substring(0, 3)) <= 649) {
+  	if (cardNumber.substring(0, 4) === '6011' || cardNumber.substring(0, 2) === '65' || prefixInRange(644, 649)) {
   		return true;
   	}
   	return false;
-  }
+	}
 
-
-
-  if (cardNumber.length === 14 && ['38', '39'].includes(cardNumber.substring(0, 2))) {
+  if (cardNumber.length === 14 && prefixInRange(38, 39)) {
   	return 'Diner\'s Club';
   } else if (cardNumber.length === 15 && ['34', '37'].includes(cardNumber.substring(0, 2))) {
   	return 'American Express';
   } else if (cardNumber.charAt(0) === '4' && [13, 16, 19].includes(cardNumber.length)) {
   	return 'Visa';
-  } else if ( (parseInt(cardNumber.substring(0, 2)) <= 55 && parseInt(cardNumber.substring(0, 2)) >= 51) && (cardNumber.length === 16) ) {
+  } else if ( prefixInRange(51, 55) && (cardNumber.length === 16) ) {
   	return 'MasterCard';
   } else if ((cardNumber.length >= 12 && cardNumber.length <= 19) && ['5018', '5020', '5038', '6304'].includes(cardNumber.substring(0, 4))) {
   	return 'Maestro'
